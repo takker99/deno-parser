54acd747ebc2025e33a02217a3c0796193778b2d
takker99 <37929109+takker99@users.noreply.github.com>
2024-08-31 09:45:04 +0900

    refactor: reduce the bundle size

--- src/bread-n-butter.ts
+++ src/bread-n-butter.ts
@@ -3,7 +3,7 @@
  */
 export class Parser<A> {
   /**
-   * The parsing action. Takes a parsing Context and returns an ActionResult
+   * The parsing action. Takes a parsing {@linkcode Context} and returns an {@linkcode ActionResult}
    * representing success or failure.
    */
   action: (context: Context) => ActionResult<A>;
@@ -22,7 +22,7 @@ export class Parser<A> {
     const location = { index: 0, line: 1, column: 1 };
     const context = new Context({ input, location });
     const result = this.skip(eof).action(context);
-    if (result.type === "ActionOK") {
+    if (isOk(result)) {
       return {
         type: "ParseOK",
         value: result.value,
@@ -45,8 +45,7 @@ export class Parser<A> {
     }
     const { expected, location } = result;
     const { line, column } = location;
-    const message =
-      `parse error at line ${line} column ${column}: ` +
+    const message = `parse error at line ${line} column ${column}: ` +
       `expected ${expected.join(", ")}`;
     throw new Error(message);
   }
@@ -55,106 +54,89 @@ export class Parser<A> {
    * Combines two parsers one after the other, yielding the results of both in
    * an array.
    */
-  and<B>(parserB: Parser<B>): Parser<[A, B]> {
-    return new Parser((context) => {
+  and = <B>(parserB: Parser<B>): Parser<[A, B]> =>
+    new Parser((context) => {
       const a = this.action(context);
-      if (a.type === "ActionFail") {
+      if (isFail(a)) {
         return a;
       }
       context = context.moveTo(a.location);
-      const b = context.merge(a, parserB.action(context));
-      if (b.type === "ActionOK") {
+      const b = merge(a, parserB.action(context));
+      if (isOk(b)) {
         const value: [A, B] = [a.value, b.value];
-        return context.merge(b, context.ok(b.location.index, value));
+        return merge(b, context.ok(b.location.index, value));
       }
       return b;
     });
-  }
 
   /** Parse both and return the value of the first */
-  skip<B>(parserB: Parser<B>): Parser<A> {
-    return this.and(parserB).map(([a]) => a);
-  }
+  skip = <B>(parserB: Parser<B>): Parser<A> =>
+    this.and(parserB).map(([a]) => a);
 
   /** Parse both and return the value of the second */
-  next<B>(parserB: Parser<B>): Parser<B> {
-    return this.and(parserB).map(([, b]) => b);
-  }
+  next = <B>(parserB: Parser<B>): Parser<B> =>
+    this.and(parserB).map(([, b]) => b);
 
   /**
    * Try to parse using the current parser. If that fails, parse using the
    * second parser.
    */
-  or<B>(parserB: Parser<B>): Parser<A | B> {
-    return new Parser<A | B>((context) => {
+  or = <B>(parserB: Parser<B>): Parser<A | B> =>
+    new Parser<A | B>((context) => {
       const a = this.action(context);
-      if (a.type === "ActionOK") {
+      if (isOk(a)) {
         return a;
       }
-      return context.merge(a, parserB.action(context));
+      return merge(a, parserB.action(context));
     });
-  }
 
   /**
    * Parse using the current parser. If it succeeds, pass the value to the
    * callback function, which returns the next parser to use.
    */
-  chain<B>(fn: (value: A) => Parser<B>): Parser<B> {
-    return new Parser((context) => {
+  chain = <B>(fn: (value: A) => Parser<B>): Parser<B> =>
+    new Parser((context) => {
       const a = this.action(context);
-      if (a.type === "ActionFail") {
+      if (isFail(a)) {
         return a;
       }
       const parserB = fn(a.value);
       context = context.moveTo(a.location);
-      return context.merge(a, parserB.action(context));
+      return merge(a, parserB.action(context));
     });
-  }
 
   /**
    * Yields the value from the parser after being called with the callback.
    */
-  map<B>(fn: (value: A) => B): Parser<B> {
-    return this.chain((a) => {
-      return ok(fn(a));
-    });
-  }
+  map = <B>(fn: (value: A) => B): Parser<B> => this.chain((a) => ok(fn(a)));
 
   /**
    * Returns the callback called with the parser.
    */
-  thru<B>(fn: (parser: this) => B): B {
-    return fn(this);
-  }
+  thru = <B>(fn: (parser: this) => B): B => fn(this);
 
   /**
    * Returns a parser which parses the same value, but discards other error
    * messages, using the ones supplied instead.
    */
-  desc(expected: string[]): Parser<A> {
-    return new Parser((context) => {
+  desc = (expected: string[]): Parser<A> =>
+    new Parser((context) => {
       const result = this.action(context);
-      if (result.type === "ActionOK") {
-        return result;
-      }
-      return { type: "ActionFail", furthest: result.furthest, expected };
+      return isOk(result) ? result : makeActionFail(result.furthest, expected);
     });
-  }
 
   /**
    * Wraps the current parser with before & after parsers.
    */
-  wrap<B, C>(before: Parser<B>, after: Parser<C>): Parser<A> {
-    return before.next(this).skip(after);
-  }
+  wrap = <B, C>(before: Parser<B>, after: Parser<C>): Parser<A> =>
+    before.next(this).skip(after);
 
   /**
    * Ignores content before and after the current parser, based on the supplied
    * parser.
    */
-  trim<B>(beforeAndAfter: Parser<B>): Parser<A> {
-    return this.wrap(beforeAndAfter, beforeAndAfter);
-  }
+  trim = <B>(beforeAndAfter: Parser<B>): Parser<A> =>
+    this.wrap(beforeAndAfter, beforeAndAfter);
 
   /**
    * Repeats the current parser between min and max times, yielding the results
@@ -170,23 +152,23 @@ export class Parser<A> {
     return new Parser((context) => {
       const items: A[] = [];
       let result = this.action(context);
-      if (result.type === "ActionFail") {
+      if (isFail(result)) {
         return result;
       }
-      while (result.type === "ActionOK" && items.length < max) {
+      while (isOk(result) && items.length < max) {
         items.push(result.value);
         if (result.location.index === context.location.index) {
           throw new Error(
-            "infinite loop detected; don't call .repeat() with parsers that can accept zero characters"
+            "infinite loop detected; don't call .repeat() with parsers that can accept zero characters",
           );
         }
         context = context.moveTo(result.location);
-        result = context.merge(result, this.action(context));
+        result = merge(result, this.action(context));
       }
-      if (result.type === "ActionFail" && items.length < min) {
+      if (isFail(result) && items.length < min) {
         return result;
       }
-      return context.merge(result, context.ok(context.location.index, items));
+      return merge(result, context.ok(context.location.index, items));
     });
   }
 
@@ -206,40 +188,33 @@ export class Parser<A> {
     if (max === 1) {
       return this.map((x) => [x]);
     }
-    return this.chain((first) => {
-      return separator
+    return this.chain((first) =>
+      separator
         .next(this)
         .repeat(min - 1, max - 1)
-        .map((rest) => {
-          return [first, ...rest];
-        });
-    });
+        .map((rest) => [first, ...rest])
+    );
   }
 
   /**
    * Returns a parser that adds name and start/end location metadata.
    */
-  node<S extends string>(name: S): Parser<ParseNode<S, A>> {
-    return all(location, this, location).map(([start, value, end]) => {
-      const type = "ParseNode";
-      return { type, name, value, start, end } as const;
-    });
-  }
+  node = <S extends string>(name: S): Parser<ParseNode<S, A>> =>
+    all(location, this, location).map(([start, value, end]) =>
+      ({ type: "ParseNode", name, value, start, end }) as const
+    );
 }
 
-function isRangeValid(min: number, max: number): boolean {
-  return (
-    min <= max &&
-    min >= 0 &&
-    max >= 0 &&
-    Number.isInteger(min) &&
-    min !== Infinity &&
-    (Number.isInteger(max) || max === Infinity)
-  );
-}
+const isRangeValid = (min: number, max: number): boolean =>
+  min <= max &&
+  min >= 0 &&
+  max >= 0 &&
+  Number.isInteger(min) &&
+  min !== Infinity &&
+  (Number.isInteger(max) || max === Infinity);
 
 /**
- * Result type from `node`. See `node` for more details.
+ * Result type from {@linkcode Parser.node}. See {@linkcode Parser.node} for more details.
  */
 export interface ParseNode<S extends string, A> {
   type: "ParseNode";
@@ -250,58 +225,51 @@ export interface ParseNode<S extends string, A> {
 }
 
 /**
- * Parser that yields the current `SourceLocation`, containing properties
+ * Parser that yields the current {@linkcode SourceLocation}, containing properties
  * `index`, `line` and `column`.
  */
-export const location = new Parser<SourceLocation>((context) => {
-  return context.ok(context.location.index, context.location);
-});
+export const location = new Parser<SourceLocation>((context) =>
+  context.ok(context.location.index, context.location)
+);
 
 /**
  * Returns a parser that yields the given value and consumes no input.
  */
-export function ok<A>(value: A): Parser<A> {
-  return new Parser((context) => {
-    return context.ok(context.location.index, value);
-  });
-}
+export const ok = <A>(value: A): Parser<A> =>
+  new Parser((context) => context.ok(context.location.index, value));
 
 /**
  * Returns a parser that fails with the given messages and consumes no input.
  */
-export function fail<A>(expected: string[]): Parser<A> {
-  return new Parser((context) => {
-    return context.fail(context.location.index, expected);
-  });
-}
+export const fail = <A>(expected: string[]): Parser<A> =>
+  new Parser((context) => context.fail(context.location.index, expected));
+
+const EOF = "<EOF>" as const;
 
 /**
  * This parser succeeds if the input has already been fully parsed.
  */
 export const eof = new Parser<"<EOF>">((context) => {
-  if (context.location.index < context.input.length) {
-    return context.fail(context.location.index, ["<EOF>"]);
-  }
-  return context.ok(context.location.index, "<EOF>");
+  const i = context.location.index;
+  return i < context.input.length ? context.fail(i, [EOF]) : context.ok(i, EOF);
 });
 
 /** Returns a parser that matches the exact text supplied. */
-export function text<A extends string>(string: A): Parser<A> {
-  return new Parser<A>((context) => {
+export const text = <A extends string>(string: A): Parser<A> =>
+  new Parser<A>((context) => {
     const start = context.location.index;
     const end = start + string.length;
-    if (context.input.slice(start, end) === string) {
+    if (context.input.slice(start, end) == string) {
       return context.ok(end, string);
     }
     return context.fail(start, [string]);
   });
-}
 
 /**
  * Returns a parser that matches the entire regular expression at the current
  * parser position.
  */
-export function match(regexp: RegExp): Parser<string> {
+export const match = (regexp: RegExp): Parser<string> => {
   for (const flag of regexp.flags) {
     switch (flag) {
       case "i": // ignoreCase
@@ -325,56 +293,48 @@ export function match(regexp: RegExp): Parser<string> {
     }
     return context.fail(start, [String(regexp)]);
   });
-}
+};
 
 /** A tuple of parsers */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// deno-lint-ignore no-explicit-any
 type ManyParsers<A extends any[]> = {
   [P in keyof A]: Parser<A[P]>;
 };
 
 /** Parse all items, returning their values in the same order. */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function all<A extends any[]>(...parsers: ManyParsers<A>): Parser<A> {
+// deno-lint-ignore no-explicit-any
+export const all = <A extends any[]>(...parsers: ManyParsers<A>): Parser<A> =>
   // TODO: This could be optimized with a custom parser, but I should probably add
   // benchmarking first to see if it really matters enough to rewrite it
-  return parsers.reduce((acc, p) => {
-    return acc.chain((array) => {
-      return p.map((value) => {
-        return [...array, value];
-      });
-    });
-  }, ok([]));
-}
+  parsers.reduce(
+    (acc, p) => acc.chain((array) => p.map((value) => [...array, value])),
+    ok([]),
+  );
 
 /** Parse using the parsers given, returning the first one that succeeds. */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function choice<Parsers extends Parser<any>[]>(
+// deno-lint-ignore no-explicit-any
+export const choice = <Parsers extends Parser<any>[]>(
   ...parsers: Parsers
-): Parser<ReturnType<Parsers[number]["tryParse"]>> {
+): Parser<ReturnType<Parsers[number]["tryParse"]>> =>
   // TODO: This could be optimized with a custom parser, but I should probably add
   // benchmarking first to see if it really matters enough to rewrite it
-  return parsers.reduce((acc, p) => {
-    return acc.or(p);
-  });
-}
+  parsers.reduce((acc, p) => acc.or(p));
 
 /**
  * Takes a lazily invoked callback that returns a parser, so you can create
  * recursive parsers.
  */
-export function lazy<A>(fn: () => Parser<A>): Parser<A> {
+export const lazy = <A>(fn: () => Parser<A>): Parser<A> => {
   // NOTE: This parsing action overwrites itself on the specified parser. We're
   // assuming that the same parser won't be returned to multiple `lazy` calls. I
   // never heard of such a thing happening in Parsimmon, and it doesn't seem
   // likely to happen here either. I assume this is faster than using variable
   // closure and an `if`-statement here, but I honestly don't know.
-  const parser: Parser<A> = new Parser((context) => {
-    parser.action = fn().action;
-    return parser.action(context);
-  });
+  const parser: Parser<A> = new Parser((context) =>
+    (parser.action = fn().action)(context)
+  );
   return parser;
-}
+};
 
 /**
  * Represents a location in the input (source code). Keeps track of `index` (for
@@ -400,9 +360,17 @@ export interface SourceLocation {
  */
 export type ActionResult<A> = ActionOK<A> | ActionFail;
 
+const ACTION_OK = "ActionOK" as const;
+const ACTION_FAIL = "ActionFail" as const;
+
+const isOk = <A>(result: ActionResult<A>): result is ActionOK<A> =>
+  result.type == ACTION_OK;
+const isFail = <A>(result: ActionResult<A>): result is ActionFail =>
+  result.type == ACTION_FAIL;
+
 /**
  * Represents a successful result from a parser's action callback. This is made
- * automatically by calling `context.ok`. Make sure to use `context.merge`
+ * automatically by calling {@linkcode Context.ok}. Make sure to use {@linkcode merge}
  * when writing a custom parser that executes multiple parser actions.
  */
 export interface ActionOK<A> {
@@ -413,9 +381,22 @@ export interface ActionOK<A> {
   expected: string[];
 }
 
+const makeActionOK = <A>(
+  location: SourceLocation,
+  value: A,
+  furthest: SourceLocation,
+  expected: string[],
+): ActionOK<A> => ({
+  type: ACTION_OK,
+  location,
+  value,
+  furthest,
+  expected,
+});
+
 /**
  * Represents a successful result from a parser's action callback. This is made
- * automatically by calling `context.ok`. Make sure to use `context.merge`
+ * automatically by calling {@linkcode Context.ok}. Make sure to use {@linkcode merge}
  * when writing a custom parser that executes multiple parser actions.
  */
 export interface ActionFail {
@@ -424,9 +405,15 @@ export interface ActionFail {
   expected: string[];
 }
 
-function union(a: string[], b: string[]): string[] {
-  return [...new Set([...a, ...b])];
-}
+const makeActionFail = (
+  furthest: SourceLocation,
+  expected: string[],
+): ActionFail => ({ type: ACTION_FAIL, furthest, expected });
+
+const union = (
+  a: string[],
+  b: string[],
+): string[] => [...new Set([...a, ...b])];
 
 /**
  * Represents the current parsing context.
@@ -445,23 +432,22 @@ class Context {
   /**
    * Returns a new context with the supplied location and the current input.
    */
-  moveTo(location: SourceLocation): Context {
-    return new Context({
+  moveTo = (location: SourceLocation): Context =>
+    new Context({
       input: this.input,
       location,
     });
-  }
 
-  private _internal_move(index: number): SourceLocation {
-    if (index === this.location.index) {
-      return this.location;
-    }
-    const start = this.location.index;
+  #internal_move(index: number): SourceLocation {
+    const location = this.location;
+    const start = location.index;
+    if (index == start) return location;
+
     const end = index;
     const chunk = this.input.slice(start, end);
-    let { line, column } = this.location;
+    let { line, column } = location;
     for (const ch of chunk) {
-      if (ch === "\n") {
+      if (ch == "\n") {
         line++;
         column = 1;
       } else {
@@ -475,57 +461,41 @@ class Context {
    * Represents a successful parse ending before the given `index`, with the
    * specified `value`.
    */
-  ok<A>(index: number, value: A): ActionResult<A> {
-    return {
-      type: "ActionOK",
-      value,
-      location: this._internal_move(index),
-      furthest: { index: -1, line: -1, column: -1 },
-      expected: [],
-    };
-  }
+  ok = <A>(index: number, value: A): ActionResult<A> =>
+    makeActionOK(this.#internal_move(index), value, {
+      index: -1,
+      line: -1,
+      column: -1,
+    }, []);
 
   /**
    * Represents a failed parse starting at the given `index`, with the specified
    * list `expected` messages (note: this list usually only has one item).
    */
-  fail<A>(index: number, expected: string[]): ActionResult<A> {
-    return {
-      type: "ActionFail",
-      furthest: this._internal_move(index),
-      expected,
-    };
-  }
-
-  /**
-   * Merge two sequential `ActionResult`s so that the `expected` and location data
-   * is preserved correctly.
-   */
-  merge<A, B>(a: ActionResult<A>, b: ActionResult<B>): ActionResult<B> {
-    if (b.furthest.index > a.furthest.index) {
-      return b;
-    }
-    const expected =
-      b.furthest.index === a.furthest.index
-        ? union(a.expected, b.expected)
-        : a.expected;
-    if (b.type === "ActionOK") {
-      return {
-        type: "ActionOK",
-        location: b.location,
-        value: b.value,
-        furthest: a.furthest,
-        expected,
-      };
-    }
-    return {
-      type: "ActionFail",
-      furthest: a.furthest,
-      expected,
-    };
-  }
+  fail = <A>(index: number, expected: string[]): ActionResult<A> =>
+    makeActionFail(this.#internal_move(index), expected);
 }
 
+export type { Context };
+
+/**
+ * Merge two sequential {@linkcode ActionResult}s so that the `expected` and location data
+ * is preserved correctly.
+ */
+export const merge = <A, B>(
+  a: ActionResult<A>,
+  b: ActionResult<B>,
+): ActionResult<B> => {
+  if (b.furthest.index > a.furthest.index) {
+    return b;
+  }
+  const expected = b.furthest.index === a.furthest.index
+    ? union(a.expected, b.expected)
+    : a.expected;
+  return isOk(b)
+    ? makeActionOK(b.location, b.value, a.furthest, expected)
+    : makeActionFail(a.furthest, expected);
+};
 /**
  * Represents a successful parse result.
  */
