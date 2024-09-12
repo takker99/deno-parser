ba8b4945f96bfbac7bce4c32640f8d4e2719bbb4
takker99 <37929109+takker99@users.noreply.github.com>
2024-09-12 17:10:35 +0900

    refactor: create `and` using `chain` and `map`

--- src/and.ts
+++ src/and.ts
@@ -1,9 +1,8 @@
-import { isOk, merge, type Parser } from "./parser.ts";
+import { chain } from "./chain.ts";
+import { map } from "./map.ts";
+import type { Parser } from "./parser.ts";
 
-export const and =
-  <A, B>(parserA: Parser<A>, parserB: Parser<B>): Parser<[A, B]> =>
-  (reader, ...prev) => {
-    const a = parserA(reader, ...prev);
-    if (!isOk(a)) return a;
-    return merge(reader, a, parserB(reader, ...a[1]), (b) => [a[3], b]);
-  };
+export const and = <A, B>(
+  parserA: Parser<A>,
+  parserB: Parser<B>,
+): Parser<[A, B]> => chain(parserA, (a) => map(parserB, (b) => [a, b]));
